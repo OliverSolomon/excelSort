c495c024f5b0ce264dc9a52ce6de03691d719a70
OliverSolomon <oliversolomonwainaina@gmail.com>
2021-03-09 21:23:04 +0300

    added spliting methods

--- noDuplicates.py
+++ noDuplicates.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from glob import glob
 
-df = pd.read_excel("Feb 2021.xlsx")
 
 def removeDups():
+    df = pd.read_excel("Feb 2021.xlsx")
     # Keep only FIRST record from set of duplicates
     df_first_record = df.drop_duplicates(subset="Date/Time", keep="first")
     #creates an excel file with sorted times
@@ -14,3 +14,31 @@ def removeDups():
 
 # removeDups()
 
+#splits the workbook to two workbooks(with the firt occurence and with the last occurence of a date)
+def splitWB():
+    dfNoDups = pd.read_excel("test.xlsx")
+    df_2_records1 = dfNoDups.drop_duplicates(subset="Date", keep="first")
+    df_2_records1.to_excel("firstDate.xlsx", index=False)
+    df_2_records2 = dfNoDups.drop_duplicates(subset="Date", keep="last")
+    df_2_records2.to_excel("lastDate.xlsx", index=False)
+
+
+#combines 2 excel files based on their header values
+def combine():
+
+    excel1 = 'firstDate.xlsx'
+    excel2 = 'lastDate.xlsx'
+
+    df1 = pd.read_excel(excel1)
+    df2 = pd.read_excel(excel2)
+
+    values1 = df1['Num', 'Department', 'Name', 'ID', 'Date/Time', 'Date', 'Time']
+    values2 = df2['Num', 'Department', 'Name', 'ID', 'Date/Time', 'Date', 'Time']
+
+    dataframes = [values1, values2]
+
+    join = pd.concat(dataframes)
+
+    join.to_excel("output.xlsx")
+
+combine()
